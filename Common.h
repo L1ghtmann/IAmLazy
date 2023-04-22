9a4847fc7a466d19c69ca8ebb8cfdb7d1c0023e3
Lightmann <lightmanns11@gmail.com>
2023-04-22 00:08:02 -0400

    Switch localization projects to submodule

--- Common.h
+++ Common.h
@@ -1,7 +1,8 @@
 #import "Log.h"
 
 // https://stackoverflow.com/a/1746920 -- NSLocalizedString(str, nil) but with en fallback
-#define localize(str) [[NSBundle mainBundle] localizedStringForKey:str value:[[NSBundle bundleWithPath:[[NSBundle mainBundle] pathForResource:@"en" ofType:@"lproj"]] localizedStringForKey:str value:@"?" table:nil] table:nil]
+#define bundle [[[NSBundle mainBundle] bundlePath] stringByAppendingPathComponent:@"Strings"]
+#define localize(str) [[NSBundle bundleWithPath:bundle] localizedStringForKey:str value:[[NSBundle bundleWithPath:[[NSBundle bundleWithPath:bundle] pathForResource:@"en" ofType:@"lproj"]] localizedStringForKey:str value:@"?" table:nil] table:nil]
 
 #define kWidth [UIScreen mainScreen].bounds.size.width
 #define kHeight [UIScreen mainScreen].bounds.size.height
