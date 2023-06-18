dc0ac8a019fc3405b1011b7e802289fc7db41ca6
Lightmann <lightmanns11@gmail.com>
2023-06-18 13:17:06 -0400

    Temporary fix for palera1n's environ not being prefixed

--- Task.c
+++ Task.c
@@ -3,6 +3,7 @@
 #include <spawn.h>
 #include <string.h>
 #include <stdlib.h>
+// #include <rootless.h>
 #include <sys/wait.h>
 #include <sys/syslog.h>
 
@@ -25,11 +26,18 @@ extern char **environ;
 
 int task(const char *args[]){
 	pid_t pid;
-	// char *PATH = getenv("PATH");
-	// print(PATH, 99);
+	// FIX: palera1n
+	// this is unprefixed by default ????
+	// print(getenv("PATH"), 99); // test
+	if(strstr(getenv("PATH"), "/var/jb") == NULL){
+		// https://github.com/opa334/Dopamine/blob/1595dbf05561e55aa36e8dd39a77ebe2a5dd00c1/Packages/Fugu15KernelExploit/Sources/Fugu15KernelExploit/oobPCI.swift#L252
+		setenv("PATH", "/sbin:/bin:/usr/sbin:/usr/bin:/var/jb/sbin:/var/jb/bin:/var/jb/usr/sbin:/var/jb/usr/bin", 1);
+	}
+	// print(getenv("PATH"), 99); // test
 #if DEBUG
 	posix_spawn_file_actions_t fd_actions;
 	posix_spawn_file_actions_init(&fd_actions);
+	// posix_spawn_file_actions_addopen(&fd_actions, 2, ROOT_PATH("/tmp/ial.log"), O_WRONLY | O_CREAT | O_APPEND, 0644);
 	posix_spawn_file_actions_addopen(&fd_actions, 2, "/tmp/ial.log", O_WRONLY | O_CREAT | O_APPEND, 0644);
 	int ret = posix_spawn(&pid, args[0], &fd_actions, NULL, (char* const*)args, environ);
 #else
